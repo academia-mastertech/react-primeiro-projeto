c9ca454ae2aa230686e1a0636547f188385855be
wesleyclzns <calazanscafe@outlook.com>
2018-11-27 20:54:02 -0200

    exclusão de arquivos desnecessarios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Caixa from './componentes/caixa/Caixa';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return ();
+    return <section>
+        <div class="main">
+            <div class="timer">
+                <span>00</span>
+                <span>00</span>
+            </div>
+
+            <div class="buttons">
+                <button>+</button>
+                <button>-</button>
+            </div>
+        </div>
+
+        <div class="buttons">
+            <button>Iniciar</button>
+            <button>Zerar</button>
+        </div>
+        <Caixa/>
+    </section>;
+
   }
 }
 
---
+++ src/componentes/caixa/Caixa.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './componentes/caixa/Caixa.css';
+
+class Caixa extends React.Component {
+  render() {
+    return <p>Coxinha</p>;
+  }
+}
+
+export default Caixa;
