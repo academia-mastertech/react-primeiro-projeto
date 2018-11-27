1c0b6c007ad4b896455b81726991ae0c09235fdb
wesleyclzns <calazanscafe@outlook.com>
2018-11-27 21:06:39 -0200

    backups segurança

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Caixa from './componentes/caixa/Caixa';
 
-class App extends Component {
-  render() {
-    return <section>
-        <div class="main">
-            <div class="timer">
-                <span>00</span>
-                <span>00</span>
-            </div>
-
-            <div class="buttons">
-                <button>+</button>
-                <button>-</button>
-            </div>
-        </div>
-
-        <div class="buttons">
-            <button>Iniciar</button>
-            <button>Zerar</button>
-        </div>
-        <Caixa/>
-    </section>;
-
+class App extends React.Component {
+  render(){
+    return <Caixa />
   }
 }
 
--- src/componentes/caixa/Caixa.js
+++ src/componentes/caixa/Caixa.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import './componentes/caixa/Caixa.css';
+import './Caixa.css';
 
-class Caixa extends React.Component {
-  render() {
-    return <p>Coxinha</p>;
-  }
+class Caixa extends React.Component{
+    render(){
+        return <section className="Caixa">
+            <div className="main">
+                <div className="timer">
+                <span>00</span>
+                <span>00</span>
+            </div>
+
+            <div className="buttons">
+                <button>+</button>
+                <button>-</button>
+            </div>
+        </div>
+
+        <div className="buttons">
+            <button>Iniciar</button>
+            <button>Zerar</button>
+        </div>
+      </section>
+    }
 }
 
 export default Caixa;
